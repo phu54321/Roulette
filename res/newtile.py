7d2d18b2ed3592ffa5de2640d309958e96d55bfe
phu54321 <phu54321@naver.com>
2018-01-27 16:55:04 +0900

    better text, visuals

--- res/newtile.py
+++ res/newtile.py
@@ -1,39 +1,44 @@
 from eudplib import *
 
-x = open('tiledata', 'rb').read()
+x = open('t1', 'rb').read()
 k = []
 for i in range(65536):
     j = b2i2(x, i * 2)
-    if j == 0x1AA3:
-        j = 0x0100
     k.append(j)
 
-x0 = 128 - 8 * 6
-y0 = 128 - 4 * 3
 
 def tindex(x, y):
     return y * 256 + x
 
-tb = [
-    32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5,
-    24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26,
-]
 
-for megax in range(12):
-    for megay in range(3):
-        n = megax * 3 + megay + 1
-        if tb.index(n) % 2 == 0:
-            t = 0x20
-        else:
-            t = 0x80
-        for subx in range(1, 8):
-            for suby in range(1, 8):
-                ti = tindex(x0 + megax * 8 + subx, y0 + (2 - megay) * 8 + suby)
-                if k[ti] != 0x47a8:
-                    k[ti] = t
-                else:
-                    k[ti] = 0x10
+def tn(a, b):
+    return a * 16 + b
+
+
+wTileNo = tn(16, 0)
+lTileNo = tn(195, 1)
+dTileNo = tn(0, 0)
+zTileNo = tn(1, 0)
+
+tConvMap = {
+    tn(195, 1): tn(8, 0),  # Light tile
+    tn(0, 0): tn(2, 0),  # Dark tile
+    tn(16, 0): tn(0, 0),  # Number font
+}
+
+
+for x in range(256):
+    for y in range(256):
+        dist = ((x - 128) ** 2 + (y - 128) ** 2) ** 0.5 / 2
+        if 54.5 <= dist <= 63.5:
+            ti = tindex(x, y)
+            k[ti] = tConvMap.get(k[ti], k[ti])
+
 
 x = b''.join(i2b2(y) for y in k)
 print(len(x))
-open('td2', 'wb').write(x)
+if len(x) == 131072:
+    schk = bytearray(open('../scenario.chk', 'rb').read())
+    tileSec = schk.index(b'TILE') + 8
+    schk[tileSec: tileSec + len(x)] = x
+    open('../scenario2.chk', 'wb').write(schk)
